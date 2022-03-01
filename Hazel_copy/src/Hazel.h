a468394793097049694b0f0fa9358388570e3875
Sinan-Karakaya <sinan.karakaya@epitech.eu>
2022-03-01 20:37:55 +0100

    feat: added entrypoint

---
+++ Hazel_copy/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by hazel apps
+
+#include "Hazel/Application.h"
+
+// -- Entry point -----------------------------
+#include "Hazel/EntryPoint.h"
+// --------------------------------------------
